d824596385be94b0deb3b900f6dd2aa1de9050f4
0verLighT <alexandre@0verlight.com>
2025-10-15 01:43:47 +0200

    chore: norme

--- src/ft_isprint.c
+++ src/ft_isprint.c
@@ -12,9 +12,9 @@
 
 #include "../include/libft.h"
 
-int ft_isprint(const int c)
+int	ft_isprint(const int c)
 {
-    if (c >= 32 && c <= 126)
-        return (1);
-    return (0);
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
 }
