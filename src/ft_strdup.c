f1ad3f54aeed01fb88808aa1bd6add3c01035b3a
0verLighT <alexandre@0verlight.com>
2025-10-15 02:21:42 +0200

    chore: atoi, strchr, strdsup

---
+++ src/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strdup.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amartel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 23:00:16 by amartel           #+#    #+#             */
+/*   Updated: 2025/10/14 23:00:17 by amartel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char    *ft_strdup(const char *s1)
+{
+    char	*dest;
+    int		i;
+
+    i = 0;
+    if (!s1)
+        return (NULL);
+    dest = malloc((ft_strlen(s1) + 1) * sizeof(char));
+    if (!dest)
+        return (NULL);
+    while (s1[i] != '\0')
+    {
+        dest[i] = s1[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
