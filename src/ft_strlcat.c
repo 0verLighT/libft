c5b913712c6fd1023679a9da3f5b3b07fc1b85dc
0verLighT <alexandre.kriby@gmail.com>
2025-10-16 18:59:35 +0200

    feat: strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -14,5 +14,27 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+    size_t	i;
+    size_t	j;
+    size_t	lensrc;
+    size_t	lendst;
 
+    lensrc = ft_strlen(src);
+    lendst = ft_strlen(dst);
+
+    if ( dstsize <= lendst)
+        return (dstsize + lensrc);
+    i = 0;
+    j = lendst;
+    while (src[i] && dstsize - lendst - 1 > 1) {
+
+        dst[lendst] = src[i];
+
+        j++;
+
+        i++;
+
+    }
+    dest[j] = '/0';
+    return (lendst + lensrc);
 }
