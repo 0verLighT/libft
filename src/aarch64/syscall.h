df54f96374a69e6413d8776bbe5d467252f5b162
0verLighT <alexandre@0verlight.com>
2026-03-21 02:03:44 +0100

    style: norm42

--- src/aarch64/syscall.h
+++ src/aarch64/syscall.h
@@ -1,8 +1,20 @@
-#ifndef SYSCALL_H
-#define SYSCALL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   syscall.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 02:03:03 by amartel           #+#    #+#             */
+/*   Updated: 2026/03/21 02:03:26 by amartel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <sys/types.h>
-#include <stddef.h>
+#ifndef SYSCALL_H
+# define SYSCALL_H
+
+# include <sys/types.h>
+# include <stddef.h>
 
 ssize_t	ft_write(unsigned int fd, const void *buf, size_t count);
 
